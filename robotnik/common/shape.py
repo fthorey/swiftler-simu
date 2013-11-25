f01f00bfe79c013956b05762ff2f0045d4b2206f
Florian Thorey <florian.thorey@gmail.com>
2013-11-26 00:36:02 +0100

    Add a shape class

---
+++ robotnik/common/shape.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+# coding: utf-8
+
+from PyQt4 import QtGui, QtCore
+
+class Shape(QtGui.QGraphicsItem):
+    """ Shape class is the basic class for all objects in the simulator
+    """
+
+    def __init__(self, name_):
+        """
+        """
+        # Call parent constructor
+        super(Shape, self).__init__()
+
+        # Name of the shape
+        self.name = name_
+
+        # Position of the shape in its own referential
+        self.pos = QtCore.QPointF(0, 0)
+
+        # Angle of the shape in its own referential
+        self.theta = 0
+
+        # Main color of the shape
+        self.color = QtGui.QColor(0, 0, 0)
+
+    # Return the name of the shape
+    def getName(self, ):
+        """
+        """
+        return self.name
+
+    # Return the position of the shape
+    def getPos(self, ):
+        """
+        """
+        return self.pos
+
+    # Set the position of the shape
+    def setPos(self, pos_):
+        """
+        """
+        self.pos = pos_
+
+    # Return the current theta angle
+    def getTheta(self, ):
+        """
+        """
+        return self.theta
+
+    # Set the position of the shape
+    def setTheta(self, theta_):
+        """
+        """
+        self.theta = theta_
+
+    # Return an estimate of the area painted by the item
+    def boundingRect(self, ):
+        """
+        """
+        return QtCore.QRectF(-50, -50, 100, 100)
+
+    # Define the accurate shape of the item
+    def shape(self, ):
+        """
+        """
+        path = QtGui.QPainterPath()
+        path.addRect(-50, -50, 100, 100);
+        return path;
+
+    # Define how to paint the shape
+    def paint(self, painter, option, widget):
+        """
+        """
+        painter.setBrush(self.color);
+        painter.drawRect(-50, -50, 100, 100)
