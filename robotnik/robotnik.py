21439a4c69c52cee486c5c81acaa69fedc9879fc
Florian Thorey <florian.thorey@gmail.com>
2013-11-26 20:37:57 +0100

    Add pixel 2 meter conversion function and scale factor

--- robotnik/robotnik.py
+++ robotnik/robotnik.py
@@ -13,6 +13,13 @@ class Robotnik(QtGui.QMainWindow):
     """ Robotnik class is the main container for the simulator
     """
 
+    # Conversion factor between pixels and meters
+    const.pix2m = 0.0002645833333333
+    const.m2pix = 3779.527559055
+
+    # Distance scale factor
+    const.scaleFactor = 1.0/2.5
+
     def __init__(self, stepDuration_):
         """
         """
@@ -42,22 +49,29 @@ class Robotnik(QtGui.QMainWindow):
         self.setCentralWidget(view);
 
         # Set the main window size
-        self.resize(800, 600)
+        self.resize(1*const.m2pix*const.scaleFactor, 0.5*const.m2pix*const.scaleFactor)
 
         # Center the main window
         self.center()
+
+        # Get screen geometry
+        screen = QtGui.QDesktopWidget().screenGeometry()
+
+        # Save screen width and height
+        const.screenWidth = screen.width() * const.pix2m
+        const.screenHeight = screen.height() * const.pix2m
 
         # Show the view on screen
         self.show()
 
         # Set a timer to handle time
-        timer = QtCore.QTimer(self)
+        # timer = QtCore.QTimer(self)
 
         # Connect timer trigger signal to world advance method
-        timer.timeout.connect(self.world.advance)
+        # timer.timeout.connect(self.world.advance)
 
         # Get a frame rate of ~100fps
-        timer.start(const.stepDuration);
+        # timer.start(const.stepDuration);
 
     # Center the main window
     def center(self, ):
@@ -82,6 +96,8 @@ if __name__ == '__main__':
     robotnik = Robotnik(10)
 
     # Create a differential drive robot
+    # Wheel radius = 2.1cm
+    # In-between wheel base length = 8.85cm
     woggle = Woggle("woggle", 0.021, 0.0885)
 
     # Add the objects to the simulator
