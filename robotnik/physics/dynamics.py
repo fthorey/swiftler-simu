4b2503cc30a6fa5b9d78a1ceabbbb6b20a8ef640
Florian Thorey <florian.thorey@gmail.com>
2013-12-01 22:39:18 +0100

    Patch differential drive dynamics

--- robotnik/physics/dynamics.py
+++ robotnik/physics/dynamics.py
@@ -8,33 +8,57 @@ class DifferentialDrive(object):
     """ DifferentialDrive class implements a differential drive behavior
     """
 
-    def __init__(self, wheelRadius_, wheelBaseLength_):
+    def __init__(self, robot_):
         """
         """
-        self.wheelRadius = wheelRadius_
-        self.wheelBaseLength = wheelBaseLength_
 
-    def update(self, robot):
+        # Robot that uses the differential drive
+        self.robot = robot_
+
+    def update(self, stepDuration_):
         """
         """
-        R = self.wheelRadius
-        L = self.wheelBaseLength
 
-        vel_l = robot.getLeftWheelSpeed()
-        vel_r = robot.getRightWheelSpeed()
+        # Make shortcuts for wheel radius and base length (m)
+        R = self.robot.getWheelRadius()
+        L = self.robot.getWheelBaseLength()
 
+        # Make shortcuts for wheels velocity (rad/s)
+        vel_l = self.robot.getLeftWheelSpeed()
+        vel_r = self.robot.getRightWheelSpeed()
+
+        # Velocity of the robot (m/s)
         v = R/2*(vel_l+vel_r)
+
+        # Angular velocity of the robot (rad/s)
         w = R/L*(vel_l-vel_r)
 
-        dt = const.stepDuration
-        theta_k = robot.getTheta()
+        # Delta integration time (s)
+        dt = stepDuration_
 
-        pos_k =  robot.pos()
+        # Current angle of the robot (rad)
+        theta_k = self.robot.getTheta()
 
+        # Position of the robot in the scene referential
+        pos_k =  self.robot.pos()
+
+        # Calculate robot's position and angle increment
         dx = v*cos(theta_k)*dt
         dy = v*sin(theta_k)*dt
         dtheta = (dt*w) % 2*pi
 
-        robot.setPos(pos_k.x() + dx, pos_k.y() + dy)
-        robot.setTheta(theta_k + dtheta)
-        robot.setRotation(degrees(theta_k + dtheta))
+        # Apply new position of the robot
+        self.robot.setPos(pos_k.x() + dx, pos_k.y() + dy)
+        self.robot.setTheta(theta_k + dtheta)
+        self.robot.setRotation(degrees(theta_k + dtheta))
+
+    def uni2Diff(self, v, w):
+        """
+        """
+        R = self.robot.getWheelRadius()
+        L = self.robot.getWheelBaseLength()
+
+        vel_l = v/R-(w*L)/(2*R);
+        vel_r = v/R+(w*L)/(2*R);
+
+        return vel_l, vel_r
