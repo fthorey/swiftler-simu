2ac156152a3d715549b1069c6337b2b6961a82a9
Florian Thorey <florian.thorey@gmail.com>
2013-12-04 20:01:43 +0100

    Add a check for the goal

--- robotnik/controller/supervisor.py
+++ robotnik/controller/supervisor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # coding: utf-8
 
+from math import degrees, sqrt
 from robots.robot import Robot
 from controller import GoToGoal
 from common import const
@@ -25,11 +26,11 @@ class Supervisor(object):
         self.stateEstimate = [self.robot.pos(), self.robot.getTheta()]
 
         # Goal expressed in the scene referential
-        self.goal = QtCore.QPointF(0.25*const.m2pix*const.scaleFactor,
-                                   0.15*const.m2pix*const.scaleFactor)
+        self.goal = QtCore.QPointF(-0.25*const.m2pix*const.scaleFactor,
+                                   -0.15*const.m2pix*const.scaleFactor)
 
         # Distance from the goal to which the robot stop
-        self.d_stop          = 0.05*const.m2pix*const.scaleFactor
+        self.stopDist          = 0.005*const.m2pix*const.scaleFactor
 
     # Select and execute the current controller
     def execute(self, stepDuration_):
@@ -40,6 +41,22 @@ class Supervisor(object):
     def updateOdometry(self, ):
         """
         """
+
+    # Check if the robot is nearby enough its goal
+    def isAtGoal(self, ):
+        """
+        """
+        # Get goal coordinates
+        xg = self.goal.x()
+        yg = self.goal.y()
+
+        # Get current robot position coordinates
+        x = self.robot.pos().x()
+        y = self.robot.pos().y()
+        cDist = sqrt((x-xg)*(x-xg) + (y-yg)*(y-yg))
+
+        return  cDist < self.stopDist
+
 
 class WoggleSupervisor(Supervisor):
     """ WoggleSupervisor is a class that provides a way to control a Woggle robot
@@ -55,6 +72,11 @@ class WoggleSupervisor(Supervisor):
     def execute(self, stepDuration_):
         """
         """
+
+        if self.isAtGoal():
+            self.robot.setWheelSpeeds(0, 0)
+            return
+
         # Execute the controller to obtain parameters to apply to the robot
         v, w = self.controller.execute(self.stateEstimate, self.goal, stepDuration_)
 
