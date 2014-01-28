519156d35df1bd30684fa0a2cdc3561ca59ce1f6
Florian Thorey <florian.thorey@gmail.com>
2014-01-28 23:35:18 +0100

    Add xml parser

---
+++ robotnik/utils/xmlreader.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# coding: utf-8
+
+import xml.etree.ElementTree as ET
+
+class XMLReader(object):
+    """
+    A class to handle reading and parsing of XML files for the simulator
+    """
