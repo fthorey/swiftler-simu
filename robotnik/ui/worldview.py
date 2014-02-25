2f5eaa8e485f8a42b5c7bcc0d4cf61df8d3de8c9
Florian Thorey <florian.thorey@gmail.com>
2014-02-25 20:51:18 +0100

    Remove unused scaling

--- robotnik/ui/worldview.py
+++ robotnik/ui/worldview.py
@@ -16,9 +16,6 @@ class WorldView(QtGui.QGraphicsView):
         # Rename scene to world
         self.world = self.scene
 
-        # Invert the world to get a correct frame to work in
-        self.scale(-1, -1)
-
     def focusOnWorld(self, ):
         """Scale the view to include all of the world (including robots).
         """
