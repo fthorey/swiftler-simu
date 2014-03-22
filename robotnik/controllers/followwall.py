41c04fc572ec2bc137bcdb9c14e439e9a3c16e46
Florian Thorey <florian.thorey@gmail.com>
2014-03-22 18:21:07 +0100

    Improve follow wall controller

--- robotnik/controllers/followwall.py
+++ robotnik/controllers/followwall.py
@@ -3,7 +3,7 @@
 
 from controllers.pidcontroller import PIDController
 import numpy as np
-from math import pi
+from math import pi, atan2, cos, sin, sqrt
 
 class FollowWall(PIDController):
     """The FollowWall class steers the robot to a direction that is a linear combination
@@ -22,21 +22,27 @@ class FollowWall(PIDController):
         """Get the direction in which the controller wants to move the robot
         as a vector.
         """
+        # Get heading direction
         direction = info_.direction
 
-        dirFactor = -1
-        if direction is 'left':
-            dirFactor = 1
+        # Angles are normalised to positive values:
+        # heading to left, means a wall has been detected by a
+        # sensor on the left side of the robot, as angles
+        # are measured positive CW, the angle must be multiplied
+        # by -1. If heading is right, the angle is always positive.
+        dirFactor = 1
+        if direction is 'right':
+            dirFactor = -1
 
         sensors = [(s, d) for s, d in zip(info_.sensors.insts, info_.sensors.dist)
-                   if (0 < (s.angle() * dirFactor) < pi) and (d < info_.sensors.rmax)]
+                   if (d < info_.sensors.rmax)]
 
-        # Now make sure they are sorted from front to back
+        # Make sure sensors are sorted from front to back
         sensors = sorted(sensors, key = lambda (p, d): abs(p.angle()))
 
         # No wall - drive a bit to the wall
         if len(sensors) == 0:
-            return np.array([0.8, dirFactor*0.6, 1])
+            return np.array([0.8, dirFactor * 0.6, 1])
 
         # Calculate vectors for each detecting sensors
         vectors = np.array([s.mapToParent(d, 0) for s, d in sensors])
@@ -44,15 +50,44 @@ class FollowWall(PIDController):
         # Only one sensor detect a wall
         if len(sensors) == 1:
             sensor = sensors[0]
+            reading = sensor[1]
             self._to_wall_vector = vectors[0]
             if self._along_wall_vector is None:
                 # We've only started, it's a corner,
                 # go perpendicular to its vector
+                self._along_wall_vector = np.array([dirFactor * self._to_wall_vector[1],
+                                                    -dirFactor * self._to_wall_vector[0],
+                                                    1])
+
+                # Which direction to go?
+                # either away from this corner or directly to it.
+                # let's blend ahead with corner:
+                theta_h = sensor[0].angle() * reading/ info_.sensors.rmax
+                return np.array([reading * cos(theta_h),
+                                 reading * sin(theta_h),
+                                 1])
+
+            else:
+                # To minimize jittering, blend with the previous
+                # reading, and don't rotate more than 0.2 rad.
+                prev_theta = atan2(self._along_wall_vector[1],
+                                   self._along_wall_vector[0])
+
+                self._along_wall_vector = np.array([dirFactor * self._to_wall_vector[1],
+                                                    -dirFactor * self._to_wall_vector[0],
+                                                    1])
+
+                this_theta = atan2(self._along_wall_vector[1],
+                                   self._along_wall_vector[0])
+
+                dtheta = prev_theta - this_theta
+                if abs(dtheta) > 0.2:
+                    dtheta *= 0.2*abs(dtheta)
+
                 self._along_wall_vector = np.array([
-                            dirFactor*self._to_wall_vector[1],
-                            -dirFactor*self._to_wall_vector[0],
+                            reading * cos(prev_theta - dtheta),
+                            reading * sin(prev_theta - dtheta),
                             1])
-                return self._along_wall_vector
 
         # More than one sensor detect the wall
         else:
@@ -61,6 +96,5 @@ class FollowWall(PIDController):
             a = vectors[-1]
             b = self._along_wall_vector
             self._to_wall_vector = a - b*np.dot(a,b)/np.dot(b,b)
-            return self._along_wall_vector
 
-        return np.array([0, 0, 1])
+        return self._along_wall_vector
